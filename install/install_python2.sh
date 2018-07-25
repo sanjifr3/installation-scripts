db1e36a7d9998e9462017d9fce3d7833771469dd
sanjif <sanjif@XPS>
2018-07-25 16:35:38 -0400

    Updated python installation scripts for aarch architecture - downloads less packages

--- install/install_python2.sh
+++ install/install_python2.sh
@@ -1,43 +1,32 @@
 #!/bin/bash
+OS_V=${OS_VERSION:-16.04}
 
 ## Upgrade pip2
 sudo pip2 install --upgrade pip
 
 sudo pip2 install python-dateutil==2.7.0
 
-## Install Python2 packages
-for PKG in \
-  numpy \
-  pandas \
-  testresources \
-  matplotlib \
-  seaborn \
-  scipy \
-  scikit-learn \
-  sklearn \
-  scikit-image \
-  lxml \
-  imutils \
-  pygame \
-  numexpr \
-  twitter \
-  nltk \
-  boto3 \
-  networkx \
-  watson-developer-cloud \
-  plotly \
-  cufflinks \
-  wolframalpha \
-  flask \
-  imblearn \
-  xgboost \
-  jupyter \
-  ipython \
-  spyder \
-  Cython \
-  h5py \
-  hyperas \
-; do sudo pip2 install -U $PKG ; done
+PACKAGES=''
+if [ $OS_V != "aarch" ]; then
+  PACKAGES=(
+    numpy pandas testresources matplotlib seaborn
+    scipy scikit-learn sklearn scikit-image lxml
+    imutils pygame numexpr twitter nltk boto3 
+    networkx watson-developer-cloud plotly 
+    cufflinks wolframalpha flask imblearn xgboost 
+    jupyter ipython spyder Cython h5py hyperas
+  )
+else
+  PACKAGES=(
+    numpy pandas testresources matplotlib seaborn
+    scipy scikit-learn sklearn scikit-image lxml
+    imutils boto3 watson-developer-cloud plotly 
+    cufflinks imblearn xgboost h5py hyperas Cython
+  )
+fi
+
+sudo pip2 install -U ${PACKAGES[*]}
+#for PKG in ${PACKAGES[*]}; do echo sudo pip2 install -U $PKG ; done
 
 # Install face recognition
 sudo pip2 install -U face_recognition
--- install/install_python3.sh
+++ install/install_python3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+OS_V=${OS_VERSION:-16.04}
 
 ## Download pip
 wget https://bootstrap.pypa.io/get-pip.py -O - | sudo python3 &&
@@ -11,42 +12,32 @@ sudo pip3 install --upgrade pip
 
 sudo pip3 install python-dateutil==2.7.0
 
+PACKAGES=''
+if [ $OS_V != "aarch" ]; then
+  PACKAGES=(
+    numpy pandas testresources matplotlib seaborn
+    scipy scikit-learn sklearn scikit-image lxml
+    imutils pygame numexpr twitter nltk boto3 
+    networkx watson-developer-cloud plotly 
+    cufflinks wolframalpha flask imblearn xgboost 
+    jupyter ipython spyder Cython h5py hyperas
+  )
+else
+  PACKAGES=(
+    numpy pandas testresources matplotlib seaborn
+    scipy scikit-learn sklearn scikit-image lxml
+    imutils boto3 watson-developer-cloud plotly 
+    cufflinks imblearn xgboost h5py hyperas Cython
+  )
+fi
+
 ## Install Python3 packages
-for PKG in \
-  numpy \
-  pandas \
-  testresources \
-  matplotlib \
-  seaborn \
-  scipy \
-  scikit-learn \
-  sklearn \
-  scikit-image \
-  lxml \
-  imutils \
-  pygame \
-  numexpr \
-  twitter \
-  nltk \
-  boto3 \
-  networkx \
-  watson-developer-cloud \
-  plotly \
-  cufflinks \
-  wolframalpha \
-  flask \
-  imblearn \
-  xgboost \
-  jupyter \
-  ipython \
-  spyder \
-  Cython \
-  h5py \
-  hyperas \
-; do sudo pip3 install -U $PKG ; done
+sudo pip2 install -U ${PACKAGES[*]}
+#for PKG in ${PACKAGES[*]}; do echo sudo pip2 install -U $PKG ; done
 
 # remove setuptool installation file
-sudo rm `dirname "$0"`/../setuptools*
-sudo rm setuptools*
+SCRIPTS_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+sudo rm $SCRIPTS_PATH/setuptools*
+sudo rm $SCRIPTS_PATH/install/setuptools*
 
 sudo pip3 install pyasn1==0.4.3
