82f439eef1848fcffe4dd0d5650b14affcbb9d85
Sanjif Raj <r.sanjif@gmail.com>
2018-08-16 13:41:22 -0400

    Removed obsolete code

--- obsolete/blueberry_install_scripts/install_dependencies_p2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-## Install amazon speech to text
-# Add aws credentials for Polly
-sudo apt-get install -y awscli &&
-mkdir -p ~/.aws/ &&
-cp ~/blueberry/src/blueberry_tts/credentials/* ~/.aws
--- obsolete/ubuntu16_install_scripts/tensorflow/scripts/packageTensorFlow.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# NVIDIA Jetson TX2
-cd $HOME/repos/tensorflow
-bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
-mv /tmp/tensorflow_pkg/tensorflow-*.whl $HOME/repos
-
